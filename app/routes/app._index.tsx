dc6937907f1067b91f5eab3076736fc0b54d4ec1
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-02-18 14:01:17 +0100

    Add default index view in app

---
+++ app/routes/app._index.tsx
@@ -0,0 +1,17 @@
+import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <p>Hello world</p>
+    </div>
+  );
+}
