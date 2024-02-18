832e11d7a816e927fc637bc7e4de8b19ec715a64
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-02-18 18:06:17 +0100

    Update header appearance

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,8 +11,33 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <div className="bg-slate-200 border-b-2 border-slate-300 flex-1 p-2">
+      <div className="bg-slate-200 border-b-2 border-slate-300 flex-1 p-2 flex gap-2 justify-between">
         <Link to="/app">App</Link>
+        <div className="flex gap-2">
+          <div className="flex gap-2">
+            <form action="/search" method="get" className="flex gap-2">
+              <input className="form-control h-10" name="inputQuery" />
+              <input
+                type="submit"
+                value="Search"
+                className="btn-primary h-10"
+              />
+            </form>
+            <div className="bg-slate-300 dark:bg-slate-900 w-[2px]"></div>
+            <Link to="/app" className="btn-primary h-10">
+              Workspace
+            </Link>
+            <Link to="/logout" className="btn-secondary h-10">
+              Logout
+            </Link>
+            <Link to="/signup" className="btn-primary h-10">
+              Sign up
+            </Link>
+            <Link to="/login" className="btn-secondary h-10">
+              Log in
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
