02f8f7a20d0e555fbee95e49827efecdcbeb6cb3
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-02-19 12:43:38 +0100

    Update workspace page appearance

--- app/routes/app.$workspaceId.$dataId.tsx
+++ app/routes/app.$workspaceId.$dataId.tsx
@@ -11,9 +11,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="flex-1 flex flex-col">
-      <div className="p-2 border-b-2 border-slate-200 bg-slate-100">
-        Hello world
-      </div>
+      <div className="p-2 border-b-2 border-slate-100 ">Hello world</div>
       <div className="flex flex-1 flex-col items-center justify-center">
         <p>Data info</p>
       </div>
