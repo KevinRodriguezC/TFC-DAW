3cc844f5484f61a01809d7012fb6673e5c54b8bd
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-02-28 19:16:04 +0100

    Add the default header to the main page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 
+import Header from "../components/header";
+
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -11,27 +13,10 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }} className="dark:text-white min-h-screen flex flex-col">
-      <div className="container-secondary-bg border-b-2 container-secondary-border p-2 flex gap-2 justify-between">
-        <h1 className="text-2xl font-bold self-center"><Link to="/app">TFC</Link></h1>
-        <div className="flex gap-2">
-          <div className="flex gap-2">
-            <form action="/search" method="get" className="flex gap-2">
-              <input className="form-control h-10" name="inputQuery" />
-              <input
-                type="submit"
-                value="Search"
-                className="btn-primary h-10"
-              />
-            </form>
-            <div className="bg-slate-300 dark:bg-slate-900 w-[2px]"></div>
-            <Link to="/app" className="btn-primary h-10">
-              Workspace
-            </Link>
-            <button className="bg-purple-600 btn-user-icon">A</button>
-          </div>
-        </div>
+      <Header username={null}/>
+      <div className="container-primary-bg flex-1 flex flex-col">
+        <h1 className="text-2xl">Hello world</h1>
       </div>
-      <div className="container-primary-bg flex-1"> Hello world</div>
     </div>
   );
 }
