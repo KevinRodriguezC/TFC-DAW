c7d5d72dd0336c64a73562251f1320096cb7792d
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-02-28 18:53:03 +0100

    Add header.ts component

---
+++ app/components/header.tsx
@@ -0,0 +1,40 @@
+import { Link } from "@remix-run/react";
+
+export function Header(user: any) {
+  return (
+    <div className="container-secondary-bg border-b-2 container-secondary-border p-2 flex gap-2 justify-between">
+      <h1 className="text-2xl font-bold self-center">
+        <Link to="/">TFC</Link>
+      </h1>
+      <div className="flex gap-2">
+        <div className="flex gap-2">
+          <form action="/search" method="get" className="flex gap-2">
+            <input className="form-control h-10" name="inputQuery" />
+            <input type="submit" value="Search" className="btn-primary h-10" />
+          </form>
+          <div className="bg-slate-300 dark:bg-slate-900 w-[2px]"></div>
+          {user ? (
+            <>
+              <Link to="/app" className="btn-primary h-10">
+                Workspace
+              </Link>
+              <button className="bg-purple-600 btn-user-icon">
+                {user.name}
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/login" className="btn-primary h-10">
+                Login
+              </Link>
+              <Link to="/signup" className="btn-primary h-10">
+                Create an account
+              </Link>
+              <button className="bg-purple-600 btn-user-icon">A</button>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
