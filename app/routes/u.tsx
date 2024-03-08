c46e6c7eea9e9fd5f59d7b852cb6550993bf7192
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-03-08 08:33:55 +0100

    Update user page

--- app/routes/u.tsx
+++ app/routes/u.tsx
@@ -1,5 +1,8 @@
 import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
-import { Link, Outlet } from "@remix-run/react";
+import { Link, Outlet, json, useLoaderData } from "@remix-run/react";
+import Header from "~/components/header";
+import { getUserInfo } from "~/model/user";
+import { getSession } from "~/sessions";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,31 +11,30 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+  let userId = session.get("userId");
+  if (!userId || !+userId) {
+    throw new Response("Error");
+  }
+  let userArray = await getUserInfo(+userId);
+  if (!userArray) {
+    throw new Response("Error");
+  }
+  let userInfo = {
+    name: userArray.name,
+    lastname: userArray.lastname ? userArray.lastname : "",
+    visibility: userArray.visibility == 0 ? false : true,
+  };
+  return json({ userId, userInfo });
+}
+
 export default function Index() {
+  let { userId, userInfo } = useLoaderData<typeof loader>();
+
   return (
     <div className="flex-1 flex flex-col dark:text-white container-primary-bg min-h-screen">
-      <div className="container-secondary-bg border-b-2 container-secondary-border p-2 flex gap-2 justify-between">
-        <h1 className="text-2xl font-bold self-center">
-          <Link to="/app">TFC</Link>
-        </h1>
-        <div className="flex gap-2">
-          <div className="flex gap-2">
-            <form action="/search" method="get" className="flex gap-2">
-              <input className="form-control h-10" name="inputQuery" />
-              <input
-                type="submit"
-                value="Search"
-                className="btn-primary h-10"
-              />
-            </form>
-            <div className="bg-slate-300 dark:bg-slate-900 w-[2px]"></div>
-            <Link to="/app" className="btn-primary h-10">
-              Workspace
-            </Link>
-            <button className="bg-purple-600 btn-user-icon">A</button>
-          </div>
-        </div>
-      </div>
+      <Header username={userId}></Header>
       <div className="xl:mx-auto xl:w-[1020px] flex flex-col gap-4 m-4 flex-1">
         <Outlet />
       </div>
