198a40efb7daf57e6fbd7ba6f04772ad337f87d8
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-03-24 14:00:15 +0100

    Format server.mjs

--- server.mjs
+++ server.mjs
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.all("*", createRequestHandler({ build }));
 
 app.listen(3000, () => {
-  if(process.env.NODE_ENV === "development"){
+  if (process.env.NODE_ENV === "development") {
     broadcastDevReady(build);
   }
   console.log("App listening on http://localhost:3000");
