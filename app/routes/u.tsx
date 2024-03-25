86626d8c8513b2536b3eb50ee9a9406292e371ec
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-03-25 10:14:54 +0100

    Update user page code

--- app/routes/u.tsx
+++ app/routes/u.tsx
@@ -1,15 +1,9 @@
 import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
-import {
-  Link,
-  Outlet,
-  json,
-  useLoaderData,
-  useRouteError,
-} from "@remix-run/react";
+import { Outlet, json, useLoaderData, useRouteError } from "@remix-run/react";
 import ErrorPage from "~/components/errorPage";
 import Header from "~/components/header";
+import { MainContainer } from "~/components/mainContainer";
 import { getUserSession } from "~/getUserSession";
-import { getUserInfo } from "~/model/user";
 import { getSession } from "~/sessions";
 
 export const meta: MetaFunction = () => {
@@ -23,15 +17,6 @@ export async function loader({ request }: LoaderFunctionArgs) {
   let { userId, userInfo } = await getUserSession(
     await getSession(request.headers.get("Cookie"))
   );
-  // let userArray = await getUserInfo(+userId);
-  // if (!userArray) {
-  //   throw new Response("Error");
-  // }
-  // let userInfo = {
-  //   name: userArray.name,
-  //   lastname: userArray.lastname ? userArray.lastname : "",
-  //   visibility: userArray.visibility == 0 ? false : true,
-  // };
   return json({ userInfo });
 }
 
@@ -45,11 +30,11 @@ export default function Index() {
   let { userInfo } = useLoaderData<typeof loader>();
 
   return (
-    <div className="flex-1 flex flex-col dark:text-white container-primary-bg min-h-screen">
+    <MainContainer>
       <Header username={userInfo.username} name={userInfo.name}></Header>
       <div className="xl:mx-auto xl:w-[1020px] flex flex-col gap-4 m-4 flex-1">
         <Outlet />
       </div>
-    </div>
+    </MainContainer>
   );
 }
