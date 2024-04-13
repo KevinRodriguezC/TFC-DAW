d37d5253b6dd8be5fbe1c27874e4c7ff62bd4508
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-04-13 16:43:38 +0200

    Update header appearance

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -13,34 +13,38 @@ export default function Header({
 
   return (
     <div className="container-secondary-bg border-b-2 container-secondary-border p-2 flex gap-2 justify-between">
-      <h1 className="text-2xl font-bold self-center">
+      <h1 className="text-2xl font-bold self-center flex-1">
         <Link to="/">TFC</Link>
       </h1>
-      <div className="flex gap-2">
-        <div className="flex gap-2">
-          <Form action="/search" method="get" className="flex gap-2">
-            <input className="form-control h-10" name="inputQuery" />
-            <input
+      {/* <div className="flex gap-2"> */}
+      <Form action="/search" method="get" className="flex gap-2">
+        <input
+          className="form-control h-10 w-96"
+          name="inputQuery"
+          placeholder={t("search")}
+        />
+        {/* <input
               type="submit"
               value={t("search")}
               className="btn-primary h-10"
-            />
-          </Form>
-          <div className="bg-slate-300 dark:bg-slate-900 w-[2px]"></div>
-          {username && name ? (
-            <UserDropdown username={username} name={name} />
-          ) : (
-            <>
-              <Link to="/login" className="btn-secondary h-10">
-                {t("login")}
-              </Link>
-              <Link to="/signup" className="btn-primary h-10">
-                {t("create_an_account")}
-              </Link>
-            </>
-          )}
-        </div>
+            /> */}
+      </Form>
+      <div className="bg-slate-300 dark:bg-slate-900 w-[2px] xl:hidden"></div>
+      <div className="flex justify-end xl:flex-1">
+        {username && name ? (
+          <UserDropdown username={username} name={name} />
+        ) : (
+          <>
+            <Link to="/login" className="btn-secondary h-10">
+              {t("login")}
+            </Link>
+            <Link to="/signup" className="btn-primary h-10">
+              {t("create_an_account")}
+            </Link>
+          </>
+        )}
       </div>
+      {/* </div> */}
     </div>
   );
 }
