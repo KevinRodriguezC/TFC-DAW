f17275f8fdf66a142be0d1404f45685abf21ac4d
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-05-07 19:00:00 +0200

    Update no directories appearance

--- app/components/workspaceSidebar.tsx
+++ app/components/workspaceSidebar.tsx
@@ -83,29 +83,12 @@ export function WorkspaceSidebar({
         </button>
       </div>
       <ul className="flex flex-col gap-2 flex-1">
-        {directories.length
-          ? directories.map((directory: any) => (
-              <li key={directory.id}>
-                {leftBarMenu == 1 ? (
-                  <NavLink className="navbarItem" to={"" + directory.id}>
-                    <div className="p-2 flex gap-2 bg-blue-600 text-white rounded-md self-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-folder-fill"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z" />
-                      </svg>
-                    </div>
-                    <div className="self-center word-wrap-anywhere">
-                      {directory.name}
-                    </div>
-                  </NavLink>
-                ) : (
-                  <NavLink className="navbarItemSmall" to={"" + directory.id}>
+        {directories.length ? (
+          directories.map((directory: any) => (
+            <li key={directory.id}>
+              {leftBarMenu == 1 ? (
+                <NavLink className="navbarItem" to={"" + directory.id}>
+                  <div className="p-2 flex gap-2 bg-blue-600 text-white rounded-md self-center">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
@@ -116,13 +99,44 @@ export function WorkspaceSidebar({
                     >
                       <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z" />
                     </svg>
-                  </NavLink>
-                )}
-              </li>
-            ))
-          : leftBarMenu
-          ? t("no_directories")
-          : ""}
+                  </div>
+                  <div className="self-center word-wrap-anywhere">
+                    {directory.name}
+                  </div>
+                </NavLink>
+              ) : (
+                <NavLink className="navbarItemSmall" to={"" + directory.id}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-folder-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.825a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3m-8.322.12q.322-.119.684-.12h5.396l-.707-.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981z" />
+                  </svg>
+                </NavLink>
+              )}
+            </li>
+          ))
+        ) : leftBarMenu ? (
+          <div className="flex flex-col items-center justify-center gap-4 flex-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="64"
+              height="64"
+              fill="currentColor"
+              className="bi bi-folder2-open"
+              viewBox="0 0 16 16"
+            >
+              <path d="M1 3.5A1.5 1.5 0 0 1 2.5 2h2.764c.958 0 1.76.56 2.311 1.184C7.985 3.648 8.48 4 9 4h4.5A1.5 1.5 0 0 1 15 5.5v.64c.57.265.94.876.856 1.546l-.64 5.124A2.5 2.5 0 0 1 12.733 15H3.266a2.5 2.5 0 0 1-2.481-2.19l-.64-5.124A1.5 1.5 0 0 1 1 6.14zM2 6h12v-.5a.5.5 0 0 0-.5-.5H9c-.964 0-1.71-.629-2.174-1.154C6.374 3.334 5.82 3 5.264 3H2.5a.5.5 0 0 0-.5.5zm-.367 1a.5.5 0 0 0-.496.562l.64 5.124A1.5 1.5 0 0 0 3.266 14h9.468a1.5 1.5 0 0 0 1.489-1.314l.64-5.124A.5.5 0 0 0 14.367 7z" />
+            </svg>
+            <p>{t("no_directories")}</p>
+          </div>
+        ) : (
+          ""
+        )}
       </ul>
       {canEdit ? (
         <Link
