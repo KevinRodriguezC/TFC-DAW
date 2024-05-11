f0dab02c1c7e254b6546692f554accdc3383d64f
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-05-11 16:20:34 +0200

    Update user dropdown

--- app/components/userDropdown.tsx
+++ app/components/userDropdown.tsx
@@ -22,9 +22,6 @@ export function UserDropdown() {
       </Menu.Button>
       <Menu.Items className="absolute bottom-0 left-0 right-0 sm:top-16 sm:right-1 sm:bottom-auto sm:left-auto p-2 bg-slate-100 dark:bg-slate-800 border-slate-200 dark:border-slate-900 border-2 rounded-t-2xl sm:rounded-2xl flex flex-col gap-2 min-w-72 z-10">
         <div className="flex gap-2">
-          {/* <div className="btn-user-icon-infobar bg-purple-600"> */}
-          {/* {user.username.charAt(0)} */}
-          {/* </div> */}
           <UserProfilePicture user={user} size={"size-11 text-lg"} />
           <div className="flex flex-col">
             <h2 className="text-xl font-bold">{user.name}</h2>
@@ -32,43 +29,33 @@ export function UserDropdown() {
           </div>
         </div>
         <Menu.Item>
-          {({ active }) => (
-            <Link className="btn-header-secondary" to="/">
-              {t("home")}
-            </Link>
-          )}
+          <Link className="btn-header-secondary" to="/">
+            {t("home")}
+          </Link>
         </Menu.Item>
         <Menu.Item>
-          {({ active }) => (
-            <Link className="btn-header-secondary" to="/w">
-              {t("workspaces")}
-            </Link>
-          )}
+          <Link className="btn-header-secondary" to="/w">
+            {t("workspaces")}
+          </Link>
         </Menu.Item>
         <Menu.Item>
-          {({ active }) => (
-            <Link className="btn-header-secondary" to={"/u/" + user.username}>
-              {t("view_profile")}
-            </Link>
-          )}
+          <Link className="btn-header-secondary" to={"/u/" + user.username}>
+            {t("view_profile")}
+          </Link>
         </Menu.Item>
         <Menu.Item>
-          {({ active }) => (
-            <Link className="btn-header-secondary" to="/settings">
-              {t("manage_account")}
-            </Link>
-          )}
+          <Link className="btn-header-secondary" to="/settings">
+            {t("manage_account")}
+          </Link>
         </Menu.Item>
         <Menu.Item>
-          {({ active }) => (
-            <Link className="btn-danger" to="/logout">
-              {t("logout")}
-            </Link>
-          )}
+          <Link className="btn-danger" to="/logout">
+            {t("logout")}
+          </Link>
         </Menu.Item>
       </Menu.Items>
     </Menu>
   ) : (
-    <div className={"btn-user-icon animate-pulse"}></div>
+    <div className={"btn-user-icon bg-gray-200/40 animate-pulse"}></div>
   );
 }
