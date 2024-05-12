8d10defa6f4d5516e1f8dbcc4f1ab667c69c8a8a
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-05-12 18:54:00 +0200

    Migrate database

---
+++ prisma/migrations/20240512165031_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `Directory` MODIFY `description` LONGTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `Events` MODIFY `value` LONGTEXT NULL;
+
+-- AlterTable
+ALTER TABLE `Workspace` MODIFY `description` LONGTEXT NULL;
