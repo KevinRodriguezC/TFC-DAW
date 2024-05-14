19d0e9cb55f49f69897c790e530c279e71180577
KevinRodriguezC <132350848+KevinRodriguezC@users.noreply.github.com>
2024-05-14 19:22:50 +0200

    Update logout page

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -4,6 +4,7 @@ import { Form, Link } from "@remix-run/react";
 
 import { getSession, destroySession } from "../sessions";
 import { useUser } from "../hooks/useUser";
+import { useTranslation } from "react-i18next";
 
 export const meta: MetaFunction = () => {
   return [
@@ -25,6 +26,8 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 export default function LogoutRoute() {
   const { logout } = useUser();
 
+  const { t } = useTranslation();
+
   return (
     <>
       <div className="flex flex-col dark:text-white min-h-screen">
@@ -33,13 +36,13 @@ export default function LogoutRoute() {
             method="post"
             className="container-primary-bg container-secondary-border border-2 rounded-md p-4 flex flex-col gap-4 min-w-96"
           >
-            <h2 className="">Are you sure you want to log out?</h2>
+            <h2 className="">{t("are_you_sure_you_want_to_log_out")}</h2>
             <div className="flex gap-2 justify-end">
               <Link to="/" className="btn-secondary">
-                Never mind
+                {t("no_go_back")}
               </Link>
               <button className="btn-primary" onClick={logout}>
-                Logout
+                {t("logout")}
               </button>
             </div>
           </Form>
